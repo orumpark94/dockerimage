24e4370bef68b300f1a19ce2411e61c52a9166ff
orumpark94 <nowd940@gmail.com>
2025-06-18 17:17:54 +0900

    인터넷 게이트웨이 생성, 퍼블릭 라우팅 테이블 생성 및 igw 연결,

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -30,3 +30,33 @@ resource "aws_subnet" "private" {
     Name = "${var.name}-private-${count.index}"
   }
 }
+
+# ✅ 인터넷 게이트웨이
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+# ✅ 퍼블릭 라우팅 테이블
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.name}-public-rt"
+  }
+}
+
+# ✅ 퍼블릭 서브넷에 라우팅 테이블 연결
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
