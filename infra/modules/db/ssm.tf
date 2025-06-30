68c7c7135885a484ebae6c7947867561d1f05671
orumpark94 <nowd940@gmail.com>
2025-06-30 14:51:58 +0900

    ssm 읽기 권한 수정

--- infra/modules/db/ssm.tf
+++ infra/modules/db/ssm.tf
@@ -1,12 +1,12 @@
 resource "aws_ssm_parameter" "db_host" {
   name  = "/app/DB_HOST"
-  type  = "String"
+  type  = "SecureString"
   value = aws_db_instance.this.address
 }
 
 resource "aws_ssm_parameter" "db_user" {
   name  = "/app/DB_USER"
-  type  = "String"
+  type  = "SecureString"
   value = var.db_user
 }
 
@@ -14,4 +14,10 @@ resource "aws_ssm_parameter" "db_password" {
   name  = "/app/DB_PASSWORD"
   type  = "SecureString"
   value = var.db_password
+}
+
+resource "aws_ssm_parameter" "db_name" {
+  name  = "/app/DB_NAME"
+  type  = "SecureString"
+  value = var.db_name
 }
