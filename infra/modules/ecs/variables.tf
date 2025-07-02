750f39b3014a387f572ecdcd2b2d765fdc5f3ebc
orumpark94 <nowd940@gmail.com>
2025-07-02 14:36:16 +0900

    전체 설정 변경

--- infra/modules/ecs/variables.tf
+++ infra/modules/ecs/variables.tf
@@ -30,8 +30,3 @@ variable "region" {
   description = "AWS region"
   type        = string
 }
-
-variable "ecs_cluster_name" {
-  description = "ECS 클러스터 이름 (CloudWatch 로그 그룹 참조용)"
-  type        = string
-}
