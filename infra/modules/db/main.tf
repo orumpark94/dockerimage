df08f35db0a08958c378fa0a49dfb1457698ce66
orumpark94 <nowd940@gmail.com>
2025-07-02 14:38:03 +0900

    전체 설정 변경

--- infra/modules/db/main.tf
+++ infra/modules/db/main.tf
@@ -45,7 +45,6 @@ resource "aws_db_instance" "this" {
   db_name                 = var.db_name
   username                = var.db_user
   password                = var.db_password
-  cloudwatch_logs_exports = ["error"]
   parameter_group_name    = aws_db_parameter_group.this.name
 
   tags = {
